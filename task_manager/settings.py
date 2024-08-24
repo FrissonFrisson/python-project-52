cc5e3e3ce96ef9ef9dd8918dd51144e1cd589aef
FrissonFrisson <compton56rus@gmail.com>
2024-08-24 17:27:23 +0500

    fix bug translate for hexlet-check test

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -125,9 +125,6 @@ LANGUAGES = [
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
-USE_L10N = True
-USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
