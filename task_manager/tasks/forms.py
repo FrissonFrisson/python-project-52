9daff9f932ecfb7d0063075fb3dbc569883dcb33
FrissonFrisson <compton56rus@gmail.com>
2024-08-24 22:24:01 +0500

    change view create_task select full_name

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -6,3 +6,7 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ('name', 'description', 'status', 'executor', 'label')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['executor'].label_from_instance = lambda obj: obj.get_full_name()
